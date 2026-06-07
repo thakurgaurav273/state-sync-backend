fbc999127537618d3f773df95f59418cbec817f1
Gaurav Singh <thakurgaurav273@gmail.com>
2026-06-07 20:51:37 +0530

    changes

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
       res.cookie("accessToken", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
       return {
